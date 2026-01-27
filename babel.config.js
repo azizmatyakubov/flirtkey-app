2fcf859347456663504c65d5ab7d3cb5c45b596a
Aziz Matyakubov <azizkieogo@gmail.com>
2026-01-27 23:08:24 +0000

    Fix: Upgrade to reanimated 4.1.1 with proper worklets support

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { worklets: false }]],
+    presets: [['babel-preset-expo', { worklets: true }]],
     plugins: [
-      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
